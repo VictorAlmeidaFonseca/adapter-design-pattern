3fd3f13c39ddccae6e373582c4185edc42cc32d2
Victor Fonseca <victor.fonseca@linkapi.com.br>
2020-09-20 21:13:55 -0300

    done

--- data-fns.js
+++ data-fns.js
@@ -3,12 +3,12 @@ const parseIso = require('date-fns/parseISO');
 
 class DataFn {
     constructor(inputData) {
-        this.inputData = inputData
+        this.inputData = inputData;
     }
-
+    
     formatToDayMonthYear() {
-        console.log(dataFn(parseIso(this.inputData), 'dd-MM-yyyy'))
+        return dataFn(parseIso(this.inputData), 'dd-MM-yyyy');
     }
 
-}
+};
  module.exports = DataFn;
